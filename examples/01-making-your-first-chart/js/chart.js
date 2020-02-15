b8449c519a3408bed36c8e713ccb452d89b2b692
Diénert Vieira <dienertalencar@gmail.com>
2020-02-15 14:33:13 -0300

    Adicionando média

--- examples/01-making-your-first-chart/js/chart.js
+++ examples/01-making-your-first-chart/js/chart.js
@@ -1,103 +1,110 @@
 async function drawLineChart() {
-  const dataset = await d3.json("./../data/seattle_wa_weather_data.json")
+    const dataset = await d3.json('./scripts/my_weather_data.json');
+    // console.table(dataset[0]);
 
-  // Access data
-  const dateFormatString = "%Y-%m-%d"
-  const dateParser = d3.timeParse(dateFormatString)
-  const xAccessor = d => dateParser(d.date)
-  const yAccessor = d => d.temperatureMax
+     // Funções de acesso
+    const yAccessor = d => d.temperatureMax;
+    const dateParser = d3.timeParse("%Y-%m-%d")
+    const xAccessor = d => dateParser(d.date);
+    console.log(xAccessor(dataset[0]))
 
-  // Create dimensions for our chart
-  // We want a small top and right margin to give our chart some space.
-  // The line of the y-axis might overflow the chart bounds, so let's
-  // define a larger bottom and left room to create room for our axes.
-  let dimensions = {
-    width: window.innerWidth * 0.9,
-    height: 400,
-    margin: {
-      top: 15,
-      right: 15,
-      bottom: 40,
-      left: 60,
-    },
-  }
-  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+    // Criando as dimensões do nosso gráfico
+    // Queremos uma pequena margem no topo e à direita
+    // A linha do eixo-y pode passar da fronteira do gráfico, então vamos
+    // definir uma margem maior em baixo e à esquerda pra dar mais espaço aos eixos
+    let dimensions = {
+        width: window.innerWidth * 0.9,
+        height: 400,
+        margin: {
+        top: 15,
+        right: 15,
+        bottom: 40,
+        left: 60,
+        },
+    }
 
-  // Draw
-  const wrapper = d3.select("#wrapper")
-    .append("svg")
-      .attr("width", dimensions.width)
-      .attr("height", dimensions.height)
+    dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
+    dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
 
-  // We can draw our chart inside of a "g" element and shift it all at once using the CSS transform property
-  const bounds = wrapper.append("g")  // Think of "g" as the "div" equivalent within an SVG element
-    .style("transform", `translate(${
-      dimensions.margin.left
-    }px, ${
-      dimensions.margin.top
-    }px)`)
+    // Desenhando
+    const svg = d3.select("#wrapper")
+        .append("svg")
+            .attr("width", dimensions.width)
+            .attr("height", dimensions.height)
 
-  // Create our scales
-  const yScale = d3.scaleLinear()
-    .domain(d3.extent(dataset, yAccessor))  // d3.extent returns an array with the min and max value
-    .range([dimensions.boundedHeight, 0])   // range refers to the highest and lowest numbers to display
+    // Podemos desenhar o gráfico dentro de um elemento "g" e movê-lo todo de uma vez usando a propriedade de transformação do CSS
+    const bounds = svg.append("g")  // Pense no "g" como o equivalente a uma "div" dentro do SVG
+        .style("transform", `translate(${dimensions.margin.left}px,
+                                       ${dimensions.margin.top}px)`)
 
-  // Let's visualize the threshold by adding a rectangle to cover all temperatures below freezing
-  const freezingTemperaturePlacement = yScale(32) // 32 degrees Fahrenheit
-  const freezingTemperatures = bounds.append("rect")
-    .attr("x", 0)
-    .attr("width", dimensions.boundedWidth)
-    .attr("y", freezingTemperaturePlacement)
-    .attr("height", dimensions.boundedHeight - freezingTemperaturePlacement)
-    .attr("fill", "#e0f3f3")  // Make the rectangle frosty to indicate freezing
+    // Criando a escala
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(dataset, yAccessor))  // d3.extent retorna um array com a temperatura mínima e máxima
+        .range([dimensions.boundedHeight, 0])   // faixa com o menor e maior número de pixels a mostrar
 
-  const xScale = d3.scaleTime()
-    .domain(d3.extent(dataset, xAccessor))  // Use a time scale since we are working with date objects
-    .range([0, dimensions.boundedWidth])
+    // Vamos visualizar a média das temperaturas em um retângulo
+    const meanTemperaturePlacement = yScale(d3.mean(dataset, yAccessor))
+    const meanTemperatures = bounds.append("rect")
+        .attr("x", 0)
+        .attr("width", dimensions.boundedWidth)
+        .attr("y", meanTemperaturePlacement)
+        .attr("height", dimensions.boundedHeight - meanTemperaturePlacement)
+        .attr("fill", "blue")  // Fazemos um retângulo para mostrar a parte mais fria
 
-  /*
-    “The d attribute will take a few commands that can be capitalized (if giving an absolute value) or lowercased (if giving a relative value):
+    // Usa uma escala de tempo, pois estamos lidando do datas
+    const xScale = d3.scaleTime()
+        .domain(d3.extent(dataset, xAccessor))  
+        .range([0, dimensions.boundedWidth])
+    
+      /*
+    “O atributo d de um path aceita alguns comandos que podem ser com letra maiúscula (se dado um valo absoluto)
+    ou minúscula (se dado um valor relativo):
 
-    M will move to a point (followed by x and y values)
-    L will draw a line to a point (followed by x and y values)
-    Z will draw a line back to the first point”
+    M move para um ponto (seguido dos valores x e y)
+    L desenha uma linha até um ponto (seguido dos valores x e y)
+    Z desenha uma linha para o prrimeiro ponto”
 
     Excerpt From: Nate Murray. “Fullstack Data Visualization with D3.” Apple Books.
 
-    bounds.append("path").attr("d", "M 0 0 L 100 0 L 100 100 L 0 50 Z")
+    bounds.append("path").attr("d", "M 0 0 L 100 0 L 100 100 L 50 100 L 50 50 L 0 50 Z")
+    
+    */
+   
+    // Cria um gerador que converte pontos de dados em uma string para o atributo d do path
+    const lineGenerator = d3.line() 
+    // Transforma o ponto para o a função de acesso apropriada e a escala para pegar o valor do 
+    // espaço em pixels
+        .x(d => xScale(xAccessor(d)))
+        .y(d => yScale(yAccessor(d)))
 
-  */
-  const lineGenerator = d3.line() // Creates a generator that converts data points into a d string
-    // Transform our data point with the appropriate accessor and the scale to get the scaled value in pixel space
-    .x(d => xScale(xAccessor(d)))
-    .y(d => yScale(yAccessor(d)))
+        const line = bounds.append("path")
+        // Alimenta a função geradora de linha como o nosso dataset
+        .attr("d", lineGenerator(dataset))
+        // Elementos SVG por padrão têm o preenchimento preto (fill) e sem traço (stroke);
+        // o que resulta em um forma preenchida, a menos que adicionamentos algum estilo
+        .attr("fill", "none")
+        .attr("stroke", "#af9358")
+        .attr("stroke-width", 2)
 
-  const line = bounds.append("path")
-    // Feed our dataset to our line generator function
-    .attr("d", lineGenerator(dataset))
-    // SVG elements default to a black fill and no stroke; which gives us a filled in shape unless we add styling
-    .attr("fill", "none")
-    .attr("stroke", "#af9358")
-    .attr("stroke-width", 2)
+    const pt_br = await d3.json('../pt-BR.json')
+    // ºC
 
-  // Additional things to draw (tick marks, labels, legends, etc)
+    const decimalFormat = d3.formatDefaultLocale(pt_br).format(".1f")
+    const timeFormat = d3.timeFormatDefaultLocale(pt_br).format("%B")
 
-  // Draw y axis tick marks and labels
-  const yAxisGenerator = d3.axisLeft()  // We want labels of the y-axis to be to the left of the axis line
-    .scale(yScale)
-  // Our axis generator will create lots of element; create a g element to contain them and keep our DOM organized
-  const yAxis = bounds.call(yAxisGenerator)
+    const yAxisGenerator = d3.axisLeft()
+        .scale(yScale)
+        .tickFormat(decimalFormat)
+    const yAxis = bounds.append("g")
+        .call(yAxisGenerator)
 
-  // Draw x axis tick marks and labels
-  const xAxisGenerator = d3.axisBottom()  // We want our labels of the x-axis to appear under the axis line
-    .scale(xScale)
-  const xAxis = bounds.append("g")
-    .call(xAxisGenerator)
-    // If you stop here, the xAxisGenerator knows how to display tick marks and labels relative to the axis line, but we need to move it to the bottom with a CSS transform
-      .style("transform", `translateY(${
-        dimensions.boundedHeight
-      }px)`)
+    const xAxisGenerator = d3.axisBottom()
+        .scale(xScale)
+        .tickFormat(timeFormat)
+    const xAxis = bounds.append("g")
+        .call(xAxisGenerator)
+            .style("transform", `translateY(${dimensions.boundedHeight}px)`)
+
 }
 
-drawLineChart()
+drawLineChart();
